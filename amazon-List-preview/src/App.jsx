9653f276d55bcf7dc61c114caa34a527f72e52cd
Avikumar <aviiiprajapati16@gmail.com>
2025-03-19 20:35:17 +0530

    extra functionality added

--- amazon-List-preview/src/App.jsx
+++ amazon-List-preview/src/App.jsx
@@ -1,6 +1,7 @@
 import Product from "./assets/Product-List/Product.jsx";
 import './App.css';
 import './assets/Product-List/Product.css';
+import Button from "./assets/Price-Box/Button.jsx";
 
 function App() {
     return (
@@ -8,8 +9,15 @@ function App() {
             <Product Title={"Mouse"} oprice={"$12"} nprice={"$7"} />
             <Product Title={"Lights"} oprice={"$40"} nprice={"$20"} />
             <Product Title={"Jeans"} oprice={"$4"} nprice={"$2"} />
+            <Button onClick={hello}/>
+
+            <p onMouseOver={hello}>hover me!</p>
         </div>
     );
+}
+
+function hello() {
+    console.log("Hello World");
 }
 
 export default App;
---
+++ amazon-List-preview/src/assets/Price-Box/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({onClick}) {
+    return (
+        <div>
+            <button onClick={onClick}>Click me !</button>
+        </div>
+    )
+};
