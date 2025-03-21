f2f3db3be0980674b0ebfb879ca099f40e486491
Avikumar <aviiiprajapati16@gmail.com>
2025-03-21 19:07:41 +0530

    functionality added

--- Ludo-Board/src/App.jsx
+++ Ludo-Board/src/App.jsx
@@ -5,26 +5,37 @@ import './App.css'
 function App() {
     let [moves, setmoves] = useState({blue: 0, red: 0, green: 0, yellow: 0});
 
-    function handleMoves() {
+    function handleMovesb() {
         setmoves({...moves , blue: moves.blue+1});
     }
 
+    function handleMover() {
+        setmoves({...moves , red: moves.red+1});
+    }
+
+    function handleMovey() {
+        setmoves({...moves, yellow: moves.yellow + 1});
+    }
+
+    function handleMoveg() {
+        setmoves({...moves, green: moves.green + 1});
+    }
 
     return (
         <div>
             <h>Game Begin</h>
 
             <p>Blue Moves ={moves.blue}</p>
-            <button style={{backgroundColor:"blue"}} onClick={handleMoves}>+1</button>
+            <button style={{backgroundColor:"blue"}} onClick={handleMovesb}>+1</button>
 
             <p>Red Moves = {moves.red}</p>
-            <button style={{backgroundColor:"Red"}}>+1</button>
+            <button style={{backgroundColor:"Red"}} onClick={handleMover}>+1</button>
 
             <p>Yellow Moves ={moves.yellow}</p>
-            <button style={{backgroundColor:"yellow" , color:"black"}}>+1</button>
+            <button style={{backgroundColor:"yellow" , color:"black"}} onClick={handleMovey}>+1</button>
 
             <p>Green Moves = {moves.green}</p>
-            <button style={{backgroundColor:"green"}}>+1</button>
+            <button style={{backgroundColor:"green"}} onClick={handleMoveg}>+1</button>
 
         </div>
     );
